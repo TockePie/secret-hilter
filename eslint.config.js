55029c13cc6b7290d7a6aa3da780c2c1cf01acd3
Maxim <144056146+TockePie@users.noreply.github.com>
2025-10-02 12:10:35 +0300

    vite fixed

--- eslint.config.js
+++ eslint.config.js
@@ -3,33 +3,27 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import simpleImportSort from 'eslint-plugin-simple-import-sort'
-import reactPlugin from 'eslint-plugin-react'
-import prettierConfig from 'eslint-config-prettier'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 
 export default defineConfig([
-  globalIgnores(['dist, vite.config.ts']),
-  ...tseslint.configs.recommended,
+  globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
+      tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-      prettierConfig
+      reactRefresh.configs.vite
     ],
     languageOptions: {
-      ecmaVersion: 2022,
+      ecmaVersion: 2020,
       globals: globals.browser
     },
     plugins: {
-      'simple-import-sort': simpleImportSort,
-      react: reactPlugin
+      'simple-import-sort': simpleImportSort
     },
     rules: {
-      quotes: ['error', 'single'],
-      semi: ['error', 'never'],
       'simple-import-sort/exports': 'error',
       'simple-import-sort/imports': [
         'error',
