b55df92d656227c6855dd20d5076cdf54ec30517
Maxim <144056146+TockePie@users.noreply.github.com>
2025-10-29 11:07:27 +0000

    little fixes

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,13 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
       '@ui': path.resolve(__dirname, './src/components/ui')
     }
+  },
+  server: {
+    watch: {
+      usePolling: true
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 5173 // you can replace this port with any port
   }
 })
