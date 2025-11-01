2158343698eb0cbebc58299c3a8a3e9ece42e081
Maxim <144056146+TockePie@users.noreply.github.com>
2025-11-01 18:26:05 +0000

    pwa integration

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,45 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto', // ensures service worker gets injected
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'Secret Hitler',
+        short_name: 'SecHit',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#f5f5f4',
+        theme_color: '#f5f5f4',
+        icons: [
+          {
+            src: '/android-launchericon-192-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/android-launchericon-512-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        navigateFallback: '/index.html' // ← this makes React Router work offline
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
