fed4dbd6e16f86602e1f8bc8ff6e1cfe26ba11f4
Maxim <144056146+TockePie@users.noreply.github.com>
2026-05-22 16:18:05 +0300

    optimization

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -28,7 +28,7 @@ export default function HomePage() {
       <main className="max-sm:standalone:pb-10 page size-full justify-between p-4">
         <div className="my-6 flex flex-col items-center gap-6">
           <img
-            src="/shadow-cabinet-logo.png"
+            src="/secret-hitler-logo.png"
             alt="Secret Hitler Logo"
             className="size-[200px] rounded-full"
           />
@@ -55,7 +55,7 @@ export default function HomePage() {
             />
           </div>
         ) : (
-          <Link to="/newgame" className="w-full">
+          <Link to="/newgame" className="w-full p-2">
             <Button size="mobile">{t('main-page.new-game-btn')}</Button>
           </Link>
         )}
