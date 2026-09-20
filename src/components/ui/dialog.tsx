490248ae70a58f19d800baf369f9384f6e0ddb39
Maxim <144056146+TockePie@users.noreply.github.com>
2026-09-20 20:49:59 +0300

    oxlint migration

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -1,6 +1,6 @@
 'use client'
-// Customized shadcn/ui component
 
+// Customized shadcn/ui component
 import * as React from 'react'
 import * as DialogPrimitive from '@radix-ui/react-dialog'
 import { XIcon } from 'lucide-react'
